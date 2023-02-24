c8758c56589952d4d9ac783f8f588da935daf13f
Rob Lewis <rob.y.lewis@gmail.com>
2023-02-23 21:59:46 -0500

    run from submission dir

--- scripts/combine_json.py
+++ scripts/combine_json.py
@@ -20,7 +20,7 @@ def main():
     has_lean, has_tex = os.path.isfile("/autograder/lean_results.json"), os.path.isfile("/autograder/tex_results.json")
 
     lean_results = read_lean_results() if has_lean else {"tests": [{"name": "No .lean file found", "score": 0, "output": "You are expected to upload exactly one .lean file!", "status": "failed"}]}
-    tex_results = read_tex_results() if has_tex else {"tests": [{"name": "No .tex file found", "score": 0, "output": "You are expected to upload exactly one .tex file!", "status": "failed"}]}
+    tex_results = read_tex_results() if has_tex else {"tests": [{"name": "No .tex file found", "score": 0, "output": "You are expected to upload at least one .tex file!", "status": "failed"}]}
 
 
     with open("results/results.json", "w") as f_out:
